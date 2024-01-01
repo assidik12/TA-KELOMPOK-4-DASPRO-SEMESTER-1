cdbac4580c8f57b06a88e80ae9724ca891ac676f
assidik12 <sofi.sidik12@gmail.com>
2024-01-01 18:55:20 +0700

    perbaikan 6

--- Program/suhu.py
+++ Program/suhu.py
@@ -5,36 +5,44 @@ def mySuhu(temperatur, suhu):
             fahrenhait = ((9/5) * suhu) + 32
             kelvin = suhu + 273.15
             print(f"""
-      Reamur          : {reamur}
-      Fahrenhait      : {fahrenhait}
-      Kelvin          : {kelvin}
+        Celcius         : {celcius}
+        Reamur          : {reamur}
+        Fahrenhait      : {fahrenhait}
+        Kelvin          : {kelvin}
                   """)
+
     elif temperatur == "reamur" or temperatur == "2":
             celcius = (5/4) * suhu
             fahrenhait = (9/4) * suhu + 32
             kelvin = (5/4) * suhu + 273
             print(f"""
-      Reamur          : {reamur}
-      Fahrenhait      : {fahrenhait}
-      Kelvin          : {kelvin}
+        Celcius         : {celcius}
+        Reamur          : {reamur}
+        Fahrenhait      : {fahrenhait}
+        Kelvin          : {kelvin}
                   """)
+
     elif temperatur == "fahrenhait"or temperatur == "3":
             celcius = 5/9 * (suhu-32)
             reamur = 4/9 * (suhu-32)
             kelvin = 5/9 * (suhu-32) + 273
             print(f"""
-      Reamur          : {reamur}
-      Fahrenhait      : {fahrenhait}
-      Kelvin          : {kelvin}
+        Celcius         : {celcius}
+        Reamur          : {reamur}
+        Fahrenhait      : {fahrenhait}
+        Kelvin          : {kelvin}
                   """)
+
     elif temperatur == "kelvin" or temperatur == "4":
             celcius =suhu - 273
             reamur = 4/5 * (suhu - 273)
             kelvin = 9/5 * (suhu-273) + 32
             print(f"""
-      Reamur          : {reamur}
-      Fahrenhait      : {fahrenhait}
-      Kelvin          : {kelvin}
+        Celcius         : {celcius}
+        Reamur          : {reamur}
+        Fahrenhait      : {fahrenhait}
+        Kelvin          : {kelvin}
                   """)
+
     else:
             print("Masukan value dengan benar!")
